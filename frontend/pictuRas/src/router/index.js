0ea19ba0ac144bac601b611aff9eac265b828b99
“NunoRodriguess” <130775733+NunoRodriguess@users.noreply.github.com>
2025-01-09 10:42:10 +0000

    Pages, not yet defined

--- frontend/pictuRas/src/router/index.js
+++ frontend/pictuRas/src/router/index.js
@@ -1,14 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Home from '../views/Home.vue';
 import NotFound from '../views/NotFound.vue';
+import Landing from '../views/Landing.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home,
+      name: 'landing',
+      component: Landing,
       props: true
     },
     {
