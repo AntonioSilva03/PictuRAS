52913386b8641f42c2c3241b003631b4f5a31ead
“NunoRodriguess” <130775733+NunoRodriguess@users.noreply.github.com>
2025-01-11 14:42:45 +0000

    add cors

--- backend/api-gateway/app.js
+++ backend/api-gateway/app.js
@@ -1,11 +1,13 @@
 import express from 'express';
+import cors from 'cors'; // Import the CORS middleware
 import 'dotenv/config';
-
 
 import apiRouter from './router/router.js';
 
-
 const app = express();
+
+// Enable CORS for all origins
+app.use(cors());
 
 // Middleware for parsing JSON
 app.use(express.json());
