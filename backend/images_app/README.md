1b34717ffa7f6f544df64ae64df1ff02402c70d1
DigoqueDigo <al19813@aebarcelos.pt>
2025-01-11 17:56:39 +0000

    Update tools and images app

--- backend/images_app/README.md
+++ backend/images_app/README.md
@@ -2,9 +2,9 @@
 
 ## EndPoints
 
-- `GET localhost:5001/images`: obter todas as imagens
-- `GET localhost:5001/images/<image_id>`: obter a imagem *image_id*
-- `GET localhost:5001/images/info/<image_id>`: obter as informações da imagem *image_id*
-- `GET localhost:5001/images/data/<image_id>`: obter os *bytes* da imagem *image_id*
-- `POST localhost:5001/images`: adicionar uma image
-- `DELETE localhost:5001/images/<image_id>`: eliminar a imagem *image_id*
+- `GET localhost:3002/images`: obter todas as imagens
+- `GET localhost:3002/images/<image_id>`: obter a imagem *image_id*
+- `GET localhost:3002/images/info/<image_id>`: obter as informações da imagem *image_id*
+- `GET localhost:3002/images/data/<image_id>`: obter os *bytes* da imagem *image_id*
+- `POST localhost:3002/images`: adicionar uma image
+- `DELETE localhost:3002/images/<image_id>`: eliminar a imagem *image_id*
--- backend/images_app/app.py
+++ backend/images_app/app.py
@@ -1,20 +1,25 @@
 import os
+from dotenv import load_dotenv # type: ignore
 from flask import Flask # type: ignore
 from mongoengine import connect # type: ignore
+from flask_cors import CORS # type: ignore
 from routes.images import images_blueprint
 
 app = Flask(__name__)
+CORS(app)
+
+load_dotenv()
 
 connect(
-    db=os.getenv('PICTURAS_DB','picturas'),
+    db=os.getenv('IMAGES_DB','picturas-images'),
     host=os.getenv('MONGO_HOST','localhost'),
-    port=int(os.getenv('MONGO_PORT', 27017)))
+    port=int(os.getenv('MONGO_PORT', 27002)))
 
 app.register_blueprint(images_blueprint, url_prefix='/images')
 
 if __name__ == '__main__':
     app.run(
-        host=os.getenv('IMAGES_FLASK_HOST', '0.0.0.0'),
-        port=int(os.getenv('IMAGES_FLASK_PORT', 5001)),
+        host=os.getenv('IMAGES_HOST', '0.0.0.0'),
+        port=int(os.getenv('IMAGES_PORT', 3002)),
         debug=True
     )
