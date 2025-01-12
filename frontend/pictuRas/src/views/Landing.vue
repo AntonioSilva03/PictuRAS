9a4af582f8d10d02d810dfe8a5033420b188dc54
Vasco_Faria <vascomfaria@ua.pt>
2025-01-12 01:08:00 +0000

    FAQ component

--- frontend/pictuRas/src/views/Landing.vue
+++ frontend/pictuRas/src/views/Landing.vue
@@ -4,7 +4,7 @@
         <div class="landing-container">
 
             <!-- Content 1 -->
-            <Content1 :isPlaying="isPlaying" @toggleVideo="toggleVideo" />
+            <Content1 :isPlaying="isPlaying" @toggleVideo="toggleVideo"  @content1Visible="handleContent1Visibility" />
             <!-- Content 2 -->
             <Content2 :isPlaying="isPlaying" @toggleVideo="toggleVideo" />
             
@@ -13,6 +13,10 @@
 
             <!-- Content 4 -->
             <Content4></Content4>
+
+            <!-- Content5 -->
+             <Content5></Content5>
+
 
            <!-- Content 6 -->
            <div class="margin-bot">
@@ -30,7 +34,8 @@
     import Content1 from '../components/Content1-LandingPage.vue';
     import Content2 from '../components/Content2-LandingPage.vue';
     import Content3 from '../components/Content3-LandingPage.vue';
-    import Content4 from '../components/Content4-LandingPage.vue'
+    import Content4 from '../components/Content4-LandingPage.vue';
+    import Content5 from '../components/Content5-LandingPage.vue';
     import Button1 from '../components/Button-style1.vue';
     import Button2 from '../components/Button-style2.vue';
     import Footer from "../components/FooterLanding.vue";
@@ -43,6 +48,7 @@
         Content2,
         Content3,
         Content4,
+        Content5,
         Button1,
         Button2,
         Footer
@@ -53,23 +59,13 @@
             showFooter: false,
         };
     },
-    mounted() {
-    window.addEventListener("scroll", this.handleScroll);
-    },
-    beforeDestroy() {
-        window.removeEventListener("scroll", this.handleScroll);
-    },
     methods: {
         toggleVideo() {
         this.isPlaying = !this.isPlaying;
         },
-        
-        handleScroll() {
-        const content1 = this.$refs.content1;
-        const rect = content1.getBoundingClientRect();
 
-        // Mostra o footer quando content1 desaparece
-        this.showFooter = rect.bottom < 0;
+        handleContent1Visibility(isVisible) {
+        this.showFooter = !isVisible;
         },
     },
     };
