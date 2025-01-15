bf862f730fd5805714018a33f19d27bce7bfa9d7
DigoqueDigo <al19813@aebarcelos.pt>
2025-01-15 19:36:32 +0000

    Improve data fetch

--- backend/ws/processor/processor.py
+++ backend/ws/processor/processor.py
@@ -1,5 +1,6 @@
 import os
 import json
+import asyncio
 from utils.fetch import *
 from utils.preparer import get_prepared_requets
 from processor.processor_worker import ProcessorWorker
@@ -9,6 +10,8 @@ load_dotenv()
 
 PROJECTS_HOST = os.getenv('PROJECTS_HOST', 'localhost')
 PROJECTS_PORT = int(os.getenv('PROJECTS_PORT', 3003))
+IMAGES_HOST = os.getenv('IMAGES_HOST', 'localhost')
+IMAGES_PORT = int(os.getenv('IMAGES_PORT', 3002))
 
 
 class Processor:
@@ -27,8 +30,8 @@ class Processor:
     async def process_project(websocket, request):
 
         project = get_project(PROJECTS_HOST, PROJECTS_PORT, request['project'])
-        images = get_project_images(PROJECTS_HOST, PROJECTS_PORT, request['project'])
-        images = {image['id']: {'data': get_image_data(PROJECTS_HOST, PROJECTS_PORT, image['id'])} for image in images}
+        images = get_project_images(IMAGES_HOST, IMAGES_PORT, request['project'])
+        images = {image['id']: {'data': get_image_data(IMAGES_HOST, IMAGES_PORT, image['id'])} for image in images}
 
         bus_requests = get_prepared_requets(project['tools'])
         print(json.dumps(bus_requests, indent=4))
@@ -38,27 +41,20 @@ class Processor:
 
 
     async def process_preview(websocket, request):
-
-        project = get_project(request['project'])
-        images = get_project_images(request['project'])
-
-        images = [(image['id'],get_image_data(image['id'])) for image in images]
-        bus_requests = get_prepared_requets(project['tools'])
-
-        processorWorker = ProcessorWorker(websocket,request['project'],bus_requests,images,'preview')
-        await processorWorker.start()
+        print(f'Not implemented')
+        await asyncio.sleep(0)
 
 
     async def process_cancel(websocket, request):
         project = request['project']
         print(f'Canceling project: {project}')
-        await websocket.send(json.dumps({'cancel': project}))
+        await asyncio.sleep(0)
 
 
     async def process_error(websocket, request):
         error = request['error']
         print(f'Error: {error}')
-        await websocket.send(json.dumps({'error': error}))
+        await asyncio.sleep(0)
 
 
     async def start(self):
--- backend/ws/utils/fetch.py
+++ backend/ws/utils/fetch.py
@@ -8,12 +8,12 @@ def get_project(host, port, project):
 
 
 def get_project_images(host, port, project):
-    response = requests.get(f'http://{host}:{port}/projects/images/{project}')
+    response = requests.get(f'http://{host}:{port}/images/project/{project}')
     response.raise_for_status()
     return response.json()
 
 
 def get_image_data(host, port, image_id):
-    response = requests.get(f'http://{host}:{port}/projects/images/data/{image_id}')
+    response = requests.get(f'http://{host}:{port}/images/data/{image_id}')
     response.raise_for_status()
     return response.content
